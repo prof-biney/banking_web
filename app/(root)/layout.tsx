a1d9f48e720cfd25f95d64c02e3be79e1b0737d1
abiney1321 <abiney1321@gmail.com>
2025-05-28 22:35:09 +0000

    work on layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Andrew", lastName: "Biney" };
+
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-scree w-full font-inter">
+      <Sidebar user={loggedIn} />
       {children}
     </main>
   );
