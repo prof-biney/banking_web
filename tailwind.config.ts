f83a6bce9b8d78843ef2b4ff22bd207f32e21a1c
abiney1321 <abiney1321@gmail.com>
2025-05-29 23:05:17 +0000

    fix border radius error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -149,11 +149,6 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
     },
   },
   plugins: [require("tailwindcss-animate")],
