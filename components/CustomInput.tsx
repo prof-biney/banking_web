6926e2fbb98928e21f95d04dec54928bee65e9c1
abiney1321 <abiney1321@gmail.com>
2025-06-02 20:42:28 +0000

    implement CustomInput

---
+++ components/CustomInput.tsx
@@ -0,0 +1,47 @@
+import {
+  FormControl,
+  FormField,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { authFormSchema } from "@/lib/utils";
+import { Control, FieldPath } from "react-hook-form";
+import { z } from "zod";
+
+type CustomInputProps = {
+  control: Control<z.infer<typeof authFormSchema>>;
+  name: FieldPath<z.infer<typeof authFormSchema>>;
+  label: string;
+  placeholder: string;
+};
+
+export default function CustomInput({
+  control,
+  name,
+  label,
+  placeholder,
+}: CustomInputProps) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex w-ful flex-col">
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                className="input-class"
+                type={name === "password" ? "password" : "text"}
+                {...field}
+              />
+            </FormControl>
+            <FormMessage className="form-message mt-2" />
+          </div>
+        </div>
+      )}
+    />
+  );
+}
