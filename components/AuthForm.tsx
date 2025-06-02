30cbca5dd8cb8f78d2003544caadfbd0a1f04c1e
abiney1321 <abiney1321@gmail.com>
2025-06-02 21:23:39 +0000

    add footer for auth form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -69,23 +69,24 @@ export default function AuthForm({ type }: { type: string }) {
       {user ? (
         <div className="flex flex-col gap-4">{/* PlaidLink */}</div>
       ) : (
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <CustomInput
-              control={form.control}
-              name="email"
-              label="Email"
-              placeholder="Enter your email"
-            />
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <CustomInput
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Enter your email"
+              />
 
-            <CustomInput
-              control={form.control}
-              name="password"
-              label="Password"
-              placeholder="Enter your password"
-            />
+              <CustomInput
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+              />
 
-            {/* <FormField
+              {/* <FormField
               control={form.control}
               name="password"
               render={({ field }) => (
@@ -105,20 +106,38 @@ export default function AuthForm({ type }: { type: string }) {
                 </div>
               )}
             /> */}
-            <Button type="submit" disabled={isLoading} className="form-btn">
-              {isLoading ? (
-                <>
-                  <Loader2 size={20} className="animate-spin" /> &nbsp;
-                  Loading...
-                </>
-              ) : type === "sign-in" ? (
-                "Sign In"
-              ) : (
-                "Sign Up"
-              )}
-            </Button>
-          </form>
-        </Form>
+
+              <div className="flex flex-col gap-4">
+                <Button type="submit" disabled={isLoading} className="form-btn">
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Sign In"
+                  ) : (
+                    "Sign Up"
+                  )}
+                </Button>
+              </div>
+            </form>
+          </Form>
+
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === "sign-in"
+                ? "Don't have an acount?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="form-link"
+            >
+              {type === "sign-in" ? "Register" : "Login"}
+            </Link>
+          </footer>
+        </>
       )}
     </section>
   );
