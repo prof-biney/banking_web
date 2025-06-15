dd6c0f09d2eaef99114cc1b9f3282d75a4640fc5
abiney1321 <abiney1321@gmail.com>
2025-06-15 00:24:47 +0000

    add sentry to project

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,15 +5,11 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://e300b41df2ffb6d4592972400a25c0d3@o4506876178464768.ingest.us.sentry.io/4507159179034624",
+  dsn: "https://a2b041eb125d81fae95ebe2d0f25d6c6@o4509499821719552.ingest.de.sentry.io/4509499825193040",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-  
 });
