7de7d0f466c9030adc7a9828079be651da23f669
abiney1321 <abiney1321@gmail.com>
2025-06-16 19:21:34 +0000

    use optional chaining to access properties

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -15,13 +15,13 @@ export default function RightSidebar({
         <div className="profile">
           <div className="profile-img">
             <span className="text-5xl font-bold text-blue-500">
-              {user.name[0]}
+              {user?.name[0]}
             </span>
           </div>
 
           <div className="profile-details">
-            <h1 className="profile-name">{user.name}</h1>
-            <p className="profile-email">{user.email}</p>
+            <h1 className="profile-name">{user?.name}</h1>
+            <p className="profile-email">{user?.email}</p>
           </div>
         </div>
       </section>
@@ -41,7 +41,7 @@ export default function RightSidebar({
               <BankCard
                 key={banks[0].$id}
                 account={banks[0]}
-                userName={user.name}
+                userName={user?.name}
                 showBalance={false}
               />
             </div>
@@ -50,7 +50,7 @@ export default function RightSidebar({
                 <BankCard
                   key={banks[1].$id}
                   account={banks[1]}
-                  userName={user.name}
+                  userName={user?.name}
                   showBalance={false}
                 />
               </div>
