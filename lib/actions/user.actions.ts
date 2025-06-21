909402874c67a964a39a381eda0651ada747beca
abiney1321 <abiney1321@gmail.com>
2025-06-21 23:51:30 +0000

    create createLinkToken function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,8 @@ import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { parseStringify } from "../utils";
 import { stringify } from "querystring";
+import { CountryCode, Products } from "plaid";
+import { plaidClient } from "@/lib/plaid";
 
 export const signIn = async ({ email, password }: signInProps) => {
   try {
@@ -74,5 +76,25 @@ export const logoutAccount = async () => {
     return true;
   } catch (error) {
     return null;
+  }
+};
+
+export const createLinkToken = async (user: User) => {
+  try {
+    const tokenParams = {
+      user: {
+        client_user_id: user.$id,
+      },
+      client_name: user.name,
+      products: ["auth"] as Products[],
+      language: "en",
+      country_codes: ["US"] as CountryCode[],
+    };
+
+    const response = await plaidClient.linkTokenCreate(tokenParams);
+
+    return parseStringify({ linkToken: response.data.link_token });
+  } catch (error) {
+    console.log(error);
   }
 };
