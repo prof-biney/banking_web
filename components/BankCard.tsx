6f48ecc19a1ef585fd2d4b4ad356fdc6bf4f1aa5
abiney1321 <abiney1321@gmail.com>
2025-06-30 22:06:47 +0000

    link card to transaction history

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -10,7 +10,10 @@ export default function BankCard({
 }: CreditCardProps) {
   return (
     <div className="flex flex-col">
-      <Link href="/" className="bank-card">
+      <Link
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
+        className="bank-card"
+      >
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">{userName}</h1>
@@ -25,7 +28,7 @@ export default function BankCard({
               <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-              ●●●● ●●●● ●●●● <span className="text-16">1234</span>
+              ●●●● ●●●● ●●●● <span className="text-16">{account?.mask}</span>
             </p>
           </article>
         </div>
