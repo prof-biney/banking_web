19d5e307d58a6cb74a9f8d31a5be6f1250cae371
abiney1321 <abiney1321@gmail.com>
2025-07-02 11:28:13 +0000

    implement top categories

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -4,8 +4,6 @@ import { getLoggedInUser } from "@/lib/actions/user.actions";
 export default async function SignUp() {
   const loggedInUser = await getLoggedInUser();
 
-  console.log(loggedInUser);
-
   return (
     <section className="flex-center size-full max-sm:px-6">
       <AuthForm type="sign-up" />
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import BankCard from "./BankCard";
+import { countTransactionCategories } from "@/lib/utils";
+import { Category } from "./Category";
 
 export default function RightSidebar({
   user,
   transactions,
   banks,
 }: RightSidebarProps) {
+  const categories: CategoryCount[] = countTransactionCategories(transactions);
+
   return (
     <aside className="right-sidebar">
       <section className="flex flex-col pb-8">
@@ -59,6 +63,14 @@ export default function RightSidebar({
             )}
           </div>
         )}
+
+        <div className="mt-10 flex flex-1 flex-col gap-6">
+          <h2 className="header-2">Top categories</h2>
+          {categories.map((category, index) => (
+            <Category key={category.name} category={category} />
+          ))}
+          <div className="space-y-5">{}</div>
+        </div>
       </section>
     </aside>
   );
