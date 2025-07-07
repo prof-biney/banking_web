e28254ac300140d2eacb237ac563cf44ff900b2f
abiney1321 <abiney1321@gmail.com>
2025-07-07 21:44:47 +0000

    remove unwanted code

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,6 @@
 import AuthForm from "@/components/AuthForm";
-import { getLoggedInUser } from "@/lib/actions/user.actions";
 
 export default async function SignUp() {
-  const loggedInUser = await getLoggedInUser();
-
   return (
     <section className="flex-center size-full max-sm:px-6">
       <AuthForm type="sign-up" />
